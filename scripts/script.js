e988b69c45e4fe2f5515c6e9797e1856fee79908
Ihab Abou Ghannam <ihababoughannam@gmail.com>
2022-09-16 22:17:18 +0300

    win conditions

--- scripts/script.js
+++ scripts/script.js
@@ -53,7 +53,42 @@ window.onload=()=>{
             score[turn]++;
         }
         else if(gridArr[6]==gridArr[7]&&gridArr[7]==gridArr[8]){
-            for(let i=3;i<6;i++){
+            for(let i=6;i<9;i++){
+                const div=document.getElementById(i);
+                div.style.background=turn;                
+            }
+            score[turn]++;
+        }
+        else if(gridArr[0]==gridArr[3]&&gridArr[3]==gridArr[6]){
+            for(let i=0;i<7;i+=3){
+                const div=document.getElementById(i);
+                div.style.background=turn;                
+            }
+            score[turn]++;
+        }
+        else if(gridArr[1]==gridArr[4]&&gridArr[4]==gridArr[7]){
+            for(let i=1;i<8;i+=3){
+                const div=document.getElementById(i);
+                div.style.background=turn;                
+            }
+            score[turn]++;
+        }
+        else if(gridArr[2]==gridArr[5]&&gridArr[5]==gridArr[8]){
+            for(let i=2;i<9;i+=3){
+                const div=document.getElementById(i);
+                div.style.background=turn;                
+            }
+            score[turn]++;
+        }
+        else if(gridArr[0]==gridArr[4]&&gridArr[4]==gridArr[8]){
+            for(let i=0;i<9;i+=4){
+                const div=document.getElementById(i);
+                div.style.background=turn;                
+            }
+            score[turn]++;
+        }
+        else if(gridArr[2]==gridArr[4]&&gridArr[4]==gridArr[6]){
+            for(let i=2;i<7;i+=2){
                 const div=document.getElementById(i);
                 div.style.background=turn;                
             }
